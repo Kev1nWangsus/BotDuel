2884c5cde49231909b1feebacf00ab30002df0c9
Shuo Wang <1875689740ws@gmail.com>
2022-10-28 00:28:35 -0700

    refactor

---
+++ backend/src/main/java/com/snakeduel/backend/controller/pk/IndexController.java
@@ -0,0 +1,16 @@
+package com.snakeduel.backend.controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pk/")
+public class IndexController {
+    @RequestMapping("index/")
+    public String index() {
+        return "pk/index.html";
+    }
+
+
+}
+
